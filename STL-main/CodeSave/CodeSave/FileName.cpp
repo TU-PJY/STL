f229e6cbf30de7c482880b10c7427b5ab67e1421
TU-PJY <viska0319@icloud.com>
2024-03-21 17:19:20 +0900

    sync

--- STL-main/CodeSave/CodeSave/FileName.cpp
+++ STL-main/CodeSave/CodeSave/FileName.cpp
@@ -1,23 +1,37 @@
 //-----------------------
-// 2024.3.7 목78
+// 2024.3.21 목78
 //
 //c++ 복습
 // 실행 파일의 메모리 영역 - STACK, DATA, CODE, free store(heap)
 //----------------------
 #include <iostream>
-#include <array>
-#include <string>
+#include <numeric>
+#include <algorithm>
 #include "save.h"
 
 using namespace std;
 
-char gc;  // DATA
-int gnum;
+// 문제
+// 사용자가 입력한 int num값을 받아 free store int[num]메모리를 확보하라.
+// 확보 메모리의 내용을 1부터 시작하여 1씩 증가하는 int 값으로 채워라.
+// 메모리의 값을 합산한 값을 출력하라
+
+
 
 int main() {
 	save("FileName.cpp");
+	while (true) {
+		int num;
 
-	array<int, 10> a;  // int 10개릐 주소 출력
-	string s{"안녕하세요"};
-	double* p = new double[10];
+		cout << ">> ";
+		cin >> num;
+
+		int* p = new int[num];
+
+		iota(p, p + num, 1);
+
+		long long sum = accumulate(p, p + num, 0);
+
+		cout << "1부터 num까지의 합계: " << sum << endl;
+	}
 }
